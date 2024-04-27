ffaaead73baab93c9d8ec85de9e43f4e5ab42c76
~ 𝐿𝑢𝑐𝑖𝑒𝑙𝑙𝑒 𝑅𝑜𝑠𝑎𝑙𝑖𝑎 𝐻. <blank1337@keemail.me>
2024-04-27 04:00:06 +0200

    :sparkles: god of time fears me (it's 4:00am)

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 sudo pacman --needed --noconfirm -S \
  extra/python-passlib \
  ansible
-ansible-galaxy collection install -r collections.yml
+ansible-galaxy collection install -r requirements.yml
 ansible-playbook playbook.yml --ask-become-pass
